cbde0e234b0c06a3102ac481e0423a19ae1d9544
Minh Anthony Tat <minhanthonytat@gmail.com>
2023-10-04 10:43:01 +1100

    style

--- eShop/src/components/NavBar/NavBar.jsx
+++ eShop/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,19 @@
 import { NavLink } from "react-router-dom";
 import React from "react";
+import styles from "./NavBar.module.scss";
 
 const NavBar = () => {
-	return (
-		<nav>
-			<h1>Title</h1>
-			<NavLink to="/">Home</NavLink>
-			<NavLink to="/products/keyboards">Keyboards</NavLink>
-			<NavLink to="/products/switches">Switches</NavLink>
-			<NavLink to="/products/deskmats">Deskmats</NavLink>
-			<NavLink to="/products/accessories">Accessories</NavLink>
-			<NavLink to="/contact">Contact</NavLink>
-		</nav>
-	);
+  return (
+    <nav className={styles.navbar}>
+      <h1>Title</h1>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/products/keyboards">Keyboards</NavLink>
+      <NavLink to="/products/switches">Switches</NavLink>
+      <NavLink to="/products/deskmats">Deskmats</NavLink>
+      <NavLink to="/products/accessories">Accessories</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+    </nav>
+  );
 };
 
 export default NavBar;

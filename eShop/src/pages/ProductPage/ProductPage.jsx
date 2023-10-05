a5ea059864453dfac4406d7691af2a511a894890
Minh Anthony Tat <minhanthonytat@gmail.com>
2023-10-06 09:39:18 +1100

    fixed productpage

--- eShop/src/pages/ProductPage/ProductPage.jsx
+++ eShop/src/pages/ProductPage/ProductPage.jsx
@@ -4,34 +4,38 @@ import { ProductsContext } from "../../context/ProductContextProvider";
 import Variations from "../../components/Variations/Variations";
 
 const ProductPage = () => {
-	const [page, setPage] = useState(null);
-	const [total, setTotal] = useState(0);
-	let { category, id } = useParams();
-	const { productsData } = useContext(ProductsContext);
+  const [page, setPage] = useState(null);
+  const [total, setTotal] = useState(0);
+  const [optionTotal, setOptionTotal] = useState(0);
+  let { category, id } = useParams();
+  const { productsData } = useContext(ProductsContext);
 
-	useEffect(() => {
-		if (!productsData) {
-			return;
-		}
-		setPage(productsData[category].find((item) => item.id == id));
-	}, []);
-	useEffect(() => {
-		console.log(page);
-		setTotal(page?.price);
-	}, [page]);
-	useEffect(() => {
-		console.log(total);
-	}, [total]);
-	return (
-		<div>
-			<img src={page?.imgLink} />
-			<div>
-				<h1>{page?.name}</h1>
-				<h4>Price: ${total}</h4>
-				<Variations page={page} setTotal={setTotal} />
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    if (!productsData) {
+      return;
+    }
+    setPage(productsData[category].find((item) => item.id == id));
+  }, []);
+  useEffect(() => {
+    console.log(page);
+    setTotal(page?.price);
+  }, [page]);
+  useEffect(() => {
+    setTotal(page?.price + optionTotal);
+  }, [optionTotal]);
+  useEffect(() => {
+    console.log(total);
+  }, [total]);
+  return (
+    <div>
+      <img src={page?.imgLink} />
+      <div>
+        <h1>{page?.name}</h1>
+        <h4>Price: ${total}</h4>
+        <Variations page={page} setOptionTotal={setOptionTotal} />
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;

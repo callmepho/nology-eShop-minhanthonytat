dc6000e7a86f67a2de8777630edfd4c314a2e527
Minh Anthony Tat <minhanthonytat@gmail.com>
2023-10-09 10:38:06 +1100

    fix stock and price

--- eShop/src/components/Variations/Variations.jsx
+++ eShop/src/components/Variations/Variations.jsx
@@ -1,59 +1,59 @@
 import { useState, useEffect } from "react";
 
 const Variations = ({ page, setTotal, total, setStock, stock }) => {
-	const [variates, setVariates] = useState([]);
-	const variations = () => {
-		for (const key in page) {
-			console.log(key);
-			if (typeof page[key] == "object") {
-				setVariates((variates) => [...variates, key]);
-			}
-		}
-	};
+  const [variates, setVariates] = useState([]);
+  const variations = () => {
+    for (const key in page) {
+      console.log(key);
+      if (typeof page[key] == "object") {
+        setVariates((variates) => [...variates, key]);
+      }
+    }
+  };
 
-	const handleSelect = (e) => {
-		let selected = page[e.target.id][e.target.value];
-		setTotal({
-			...total,
-			[e.target.id]: selected?.price,
-		});
-		setStock({ ...stock, [e.target.id]: selected?.stock });
-	};
+  const handleSelect = (e) => {
+    let selected = page[e.target.id][e.target.value];
+    setTotal({
+      ...total,
+      [e.target.id]: selected?.price,
+    });
+    setStock({ ...stock, [e.target.id]: selected?.stock });
+  };
 
-	const options = (variate) => {
-		const keys = Object.keys(page[variate]);
-		const option = keys.map((item, index) => (
-			<option key={index} value={item}>
-				{item}
-			</option>
-		));
-		option.unshift(
-			<option selected disabled>
-				Choose here
-			</option>
-		);
-		return option;
-	};
+  const options = (variate) => {
+    const keys = Object.keys(page[variate]);
+    const option = keys.map((item, index) => (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    ));
+    option.unshift(
+      <option value="" selected disabled>
+        Choose here
+      </option>
+    );
+    return option;
+  };
 
-	useEffect(() => {
-		variations();
-	}, [page]);
+  useEffect(() => {
+    variations();
+  }, [page]);
 
-	return (
-		<div>
-			{variates.length > 0 &&
-				variates.map((variate) => {
-					return (
-						<div>
-							<label htmlFor={variate}>{variate}: </label>
-							<select id={variate} onChange={handleSelect}>
-								{options(variate)}
-							</select>
-						</div>
-					);
-				})}
-		</div>
-	);
+  return (
+    <div>
+      {variates.length > 0 &&
+        variates.map((variate) => {
+          return (
+            <div>
+              <label htmlFor={variate}>{variate}: </label>
+              <select id={variate} onChange={handleSelect} required>
+                {options(variate)}
+              </select>
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Variations;
--- eShop/src/pages/ProductPage/ProductPage.jsx
+++ eShop/src/pages/ProductPage/ProductPage.jsx
@@ -2,84 +2,103 @@ import { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { ProductsContext } from "../../context/ProductContextProvider";
 import Variations from "../../components/Variations/Variations";
+import styles from "./ProductPage.module.scss";
 
 const ProductPage = () => {
-	const [page, setPage] = useState(null);
-	const [total, setTotal] = useState({});
-	const [stock, setStock] = useState({});
-	const [quantity, setQuantity] = useState(1);
-	let { category, id } = useParams();
-	const { productsData } = useContext(ProductsContext);
+  const [page, setPage] = useState(null);
+  const [total, setTotal] = useState({});
+  const [stock, setStock] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [error, setError] = useState(null);
+  const [cart, setCart] = useState(null);
+  let { category, id } = useParams();
+  const { productsData } = useContext(ProductsContext);
 
-	const getTotal = () => {
-		return Object.values(total).reduce((init, next) => init + next, 0);
-	};
+  const getTotal = () => {
+    return Object.values(total).reduce((init, next) => init + next, 0);
+  };
 
-	const getStock = () => {
-		if (Object.keys(stock).length > 0) {
-			return Object.values(stock).reduce((init, next) =>
-				init > next ? next : init
-			);
-		} else {
-			return null;
-		}
-	};
+  const getStock = () => {
+    if (Object.keys(stock).length > 0) {
+      return Object.values(stock).reduce((init, next) =>
+        init > next ? next : init
+      );
+    } else {
+      return null;
+    }
+  };
 
-	const handleQuantity = (e) => {
-		setQuantity(e.target.value);
-		if (quantity < 0) {
-			setQuantity("1");
-		}
-	};
+  const handleQuantity = (e) => {
+    setQuantity(e.target.value);
+    if (quantity < 0) {
+      setQuantity("1");
+    }
+  };
 
-	const handleSubmit = () => {
-		if (quantity > getStock()) {
-			setQuantity(`${getStock()}`);
-		}
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    setError(null);
+    setCart(null);
+    if (quantity > getStock()) {
+      setQuantity(`${getStock()}`);
+      setError("Not enough stock.");
+    } else if (quantity < 0) {
+      setQuantity(1);
+      setError(`${quantity} invalid quantity.`);
+    } else if (quantity <= getStock()) {
+      setCart(true);
+    }
+  };
 
-	useEffect(() => {
-		if (!productsData) {
-			return;
-		}
-		setPage(productsData[category].find((item) => item.id == id));
-	}, []);
-	useEffect(() => {
-		console.log(page);
-		if (page?.price) {
-			setTotal({ ...total, base: page?.price });
-		}
-		if (page?.stock) {
-			setStock({ ...stock, base: page?.stock });
-		}
-	}, [page]);
-	useEffect(() => {
-		console.log(quantity);
-	}, [total, stock, quantity]);
+  useEffect(() => {
+    if (!productsData) {
+      return;
+    }
+    setPage(productsData[category].find((item) => item.id == id));
+  }, []);
+  useEffect(() => {
+    console.log(page);
+    if (page?.price) {
+      setTotal({ ...total, base: page?.price });
+    }
+    if (page?.stock) {
+      setStock({ ...stock, base: page?.stock });
+    }
+  }, [page]);
 
-	return (
-		<div>
-			<img src={page?.imgLink} />
-			<div>
-				<h1>{page?.name}</h1>
-				<Variations
-					page={page}
-					setTotal={setTotal}
-					total={total}
-					setStock={setStock}
-					stock={stock}
-				/>
-				<h3>Price: ${getTotal()}</h3>
-				{getStock() && <h4>Stock: {getStock()}</h4>}
-				<input
-					min="1"
-					value={quantity}
-					max={getStock()}
-					onChange={handleQuantity}></input>
-				<button onClick={handleSubmit}>test</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.page}>
+      <img className={styles.page_img} src={page?.imgLink} />
+      <div className={styles.page_info}>
+        <h1>{page?.name}</h1>
+        <form onSubmit={handleSubmit}>
+          <Variations
+            page={page}
+            setTotal={setTotal}
+            total={total}
+            setStock={setStock}
+            stock={stock}
+          />
+          <h3>Price: ${getTotal()}</h3>
+          {getStock() && <h4>Stock: {getStock()}</h4>}
+          <label htmlFor="quantity">Quantity: </label>
+          <input
+            id="quantity"
+            name="quantity"
+            min="1"
+            value={quantity}
+            max={getStock()}
+            onChange={handleQuantity}
+            className={styles.quantity}
+            required></input>
+          <button type="submit">test</button>
+        </form>
+        {error && <p>{error}</p>}
+        {cart && <p>Add to cart.</p>}
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
